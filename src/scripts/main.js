cc183273c99ea16faf47c07ccf10a00b81c18630
Claudine <claudine.bananal@gmail.com>
2016-05-02 18:09:29 -0700

    add ajax loader

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   $('.section-options').change('option', function(){
 
     event.preventDefault();
+    $('.main-content').prepend('<img class="loader" src="src/images/ajax-loader.gif">');
 
     //Stores the value of the selected news section
     var chosenSection = $(this).val();
@@ -19,21 +20,35 @@ $(document).ready(function(){
 
     //What to do upon API retrieval
     .done ( function(data) {
+
+      //Remove loader image
+      $('.loader').remove();
+
       var nytData = data.results;
       // console.log(nytData);
-      var featured = 0;
+      // var featured = 0;
 
       nytData = nytData.filter(function (item){
         return item.multimedia.length > 0;
         }).splice (0,12);
 
       for (var i = 0; i < nytData.length; i++) {
-          var featuredPhoto = nytData[i].multimedia[4].url;
-          $('.news-container').append('<img src="' + featuredPhoto + '"/>');
+          var featuredPhoto = nytData[i].multimedia[2].url;
+          var newsAbstract = nytData[i].abstract;
+          var linkToFull = nytData[i].url;
 
-          // console.log(featuredPhoto)
+          $('.news-container').append('<li><a href="' + linkToFull + '"><p>' + newsAbstract + '</p></a></li>');
+          // $('li').parent('.news-container').addcss
+
+          // console.log(featuredPhoto);
+          // console.log(newsAbstract);
+          console.log(linkToFull);
       } //end for loop
+
     }); // end for done
+
+
+
 
   }); //end for event change
 
