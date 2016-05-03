5208a4efa8c5a7493b0251a1105d15c6335ac817
Claudine <claudine.bananal@gmail.com>
2016-05-02 20:12:12 -0700

    add target attribute to links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
           var newsAbstract = nytData[i].abstract;
           var linkToFull = nytData[i].url;
 
-          $('.news-container').append('<li class="outer-square" style="background-image: url(\'' + featuredPhoto + '\');"><a class="inner-square" href="' + linkToFull + '"><p>' + newsAbstract + '</p></a></li>');
+          $('.news-container').append('<li class="outer-square" style="background-image: url(\'' + featuredPhoto + '\');"><a class="inner-square" href="' + linkToFull + '" target="_blank"><p>' + newsAbstract + '</p></a></li>');
 
           console.log(featuredPhoto);
           // console.log(newsAbstract);
